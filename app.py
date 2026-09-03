a3c03353e8a95fc5c00e70a318d5b707da9a8ecc
santachan3690 <165402179+santachan3690@users.noreply.github.com>
2026-09-04 01:10:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if prompt := st.chat_input("Nhập câu hỏi hoặc câu trả lời của em �
     with st.chat_message("assistant"):
         with st.spinner("AI đang suy nghĩ..."):
             response = client.models.generate_content(
-                model='gemini-1.5-flash',
+                model='gemini-2.5-flash',
                 contents=st.session_state.api_contents,
                 config=types.GenerateContentConfig(
                     system_instruction=SYSTEM_PROMPT,
